fd1eaba5ba8ef83570d25cb70b57cc8a1b058434
Dominik Zig <dominikzig@hotmail.co.nz>
2020-04-27 11:55:41 +1200

    Updates

---
+++ precache-manifest.30d028248493135fa004c7efb2c4f2cc.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64682317037db0cdcc9a52b0de2414c5",
+    "url": "/form-tracker/index.html"
+  },
+  {
+    "revision": "331c10252476cb6b5835",
+    "url": "/form-tracker/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "1c57f40f576b89063928",
+    "url": "/form-tracker/static/css/main.222736d0.chunk.css"
+  },
+  {
+    "revision": "331c10252476cb6b5835",
+    "url": "/form-tracker/static/js/2.70a08ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/form-tracker/static/js/2.70a08ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c57f40f576b89063928",
+    "url": "/form-tracker/static/js/main.c4f80854.chunk.js"
+  },
+  {
+    "revision": "d0fb0bc7bf239bb7b6f0",
+    "url": "/form-tracker/static/js/runtime-main.a219c3fd.js"
+  },
+  {
+    "revision": "04772e740ec599750173b29d12bff402",
+    "url": "/form-tracker/static/media/bentancur-pose.04772e74.png"
+  },
+  {
+    "revision": "050a8db562da0ed2288c7df866916d80",
+    "url": "/form-tracker/static/media/bernardeschi-pose.050a8db5.png"
+  },
+  {
+    "revision": "df8d41471414d9314ff6f386a3b11a78",
+    "url": "/form-tracker/static/media/bonucci-pose.df8d4147.png"
+  },
+  {
+    "revision": "6159479e67fc0848e0116a8c760f3a33",
+    "url": "/form-tracker/static/media/bonucci.6159479e.png"
+  },
+  {
+    "revision": "720d10026cf366f7f65c4ee7889e36f7",
+    "url": "/form-tracker/static/media/buffon-pose.720d1002.png"
+  },
+  {
+    "revision": "9526c0b1757c1d36a264560149f8d3b6",
+    "url": "/form-tracker/static/media/cuadrado-pose.9526c0b1.png"
+  },
+  {
+    "revision": "713dd537e427ea31159d8c5ecfb4c891",
+    "url": "/form-tracker/static/media/danilo-pose.713dd537.png"
+  },
+  {
+    "revision": "57d3c8a91ddd91575a0a60da5cb52e5f",
+    "url": "/form-tracker/static/media/danilo.57d3c8a9.png"
+  },
+  {
+    "revision": "eeb4c6bcb049749f8b3ce3aef19ed42d",
+    "url": "/form-tracker/static/media/deligt-pose.eeb4c6bc.png"
+  },
+  {
+    "revision": "3d2556c418e6cde3818ff74b4e821081",
+    "url": "/form-tracker/static/media/demiral-pose.3d2556c4.png"
+  },
+  {
+    "revision": "f2b1ddd80fb676c7c90d65da27a9ff66",
+    "url": "/form-tracker/static/media/dybala-pose.f2b1ddd8.png"
+  },
+  {
+    "revision": "6f8a100a891eee4818e54b9694d2fffc",
+    "url": "/form-tracker/static/media/dybala.6f8a100a.png"
+  },
+  {
+    "revision": "0d7b95df00b2ba07a2bc6f303c8c71ce",
+    "url": "/form-tracker/static/media/higuain-pose.0d7b95df.png"
+  },
+  {
+    "revision": "b352c257b4ecc25acf62098fec139b00",
+    "url": "/form-tracker/static/media/higuain.b352c257.webp"
+  },
+  {
+    "revision": "d6552d43e8925ec28ad1e0cb81c279e9",
+    "url": "/form-tracker/static/media/khedira-pose.d6552d43.png"
+  },
+  {
+    "revision": "afcc4e748a5fe74f148a849434ce7153",
+    "url": "/form-tracker/static/media/matuidi-pose.afcc4e74.png"
+  },
+  {
+    "revision": "fc7f191568dc6e7100569647e7256265",
+    "url": "/form-tracker/static/media/matuidi.fc7f1915.png"
+  },
+  {
+    "revision": "2385324dfd906a27ec0d95fddb814e26",
+    "url": "/form-tracker/static/media/rabiot-pose.2385324d.png"
+  },
+  {
+    "revision": "2d27e72c053d23b3e7ab6311711cc8fb",
+    "url": "/form-tracker/static/media/ramsey-pose.2d27e72c.png"
+  },
+  {
+    "revision": "373953bf4c3e8b7dc5f3cc170490de9e",
+    "url": "/form-tracker/static/media/ramsey.373953bf.png"
+  },
+  {
+    "revision": "18c889939f3433215c3eb2efd28a0586",
+    "url": "/form-tracker/static/media/ronaldo-pose.18c88993.png"
+  },
+  {
+    "revision": "31a411fb3564fa43056462d61f31c8b4",
+    "url": "/form-tracker/static/media/rugani-pose.31a411fb.png"
+  },
+  {
+    "revision": "597276f1170a5f9898079e90d529f23e",
+    "url": "/form-tracker/static/media/rugani.597276f1.png"
+  },
+  {
+    "revision": "2a315ea48485b0336f6469b81e1810e7",
+    "url": "/form-tracker/static/media/sandro-pose.2a315ea4.png"
+  },
+  {
+    "revision": "b8af36d955123c1e21ce90a33d167034",
+    "url": "/form-tracker/static/media/szczesny-pose.b8af36d9.png"
+  }
+]);
